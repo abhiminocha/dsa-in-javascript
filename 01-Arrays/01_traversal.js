1186eda22859e8c97024d1586e503bb6bc1f9f79
abhiminocha <124769105+abhiminocha@users.noreply.github.com>
2026-07-23 13:29:05 +0000

    Added array traversal and insertion in JS

---
+++ 01-Arrays/01_traversal.js
@@ -0,0 +1,6 @@
+// Traversal of an Array
+
+let arr = [0, 1, 2, 3];
+for (let i = 0; i < arr.length; i++) {
+  console.log(`Element at index ${i} is ${arr[i]}`);
+}
---
+++ 01-Arrays/02_insertion.js
@@ -0,0 +1,5 @@
+// Insertion of an array
+
+let numbers = [1, 2, 4, 5];
+numbers.splice(2, 0, 3);
+console.log(numbers);
