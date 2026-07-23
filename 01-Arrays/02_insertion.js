acde13b1caa9f01f69ceb1f1fc5c70bbcac75ba4
abhiminocha <124769105+abhiminocha@users.noreply.github.com>
2026-07-23 13:56:06 +0000

    Updated manual insertion

--- 01-Arrays/02_insertion.js
+++ 01-Arrays/02_insertion.js
@@ -1,5 +1,21 @@
 // Insertion of an array
 
-let numbers = [1, 2, 4, 5];
-numbers.splice(2, 0, 3);
-console.log(numbers);
+// Method 1 : Manual DSA Logic(Loop & Shiting) 
+
+function insertAtIndex(arr, element, index) {
+    for(let i = arr.length - 1; i >= index; i--){
+        arr[ i + 1 ] = arr[i]
+    }
+    arr[index] = element;
+    return arr;
+}
+
+let numbers = [1,2,4,5];
+console.log("Manual Insertion : ", insertAtIndex(numbers, 3, 2))
+
+
+// Method 2 : JS Built-in Shortcut
+
+let count = [1, 2, 4, 5];
+count.splice(2, 0, 3);
+console.log(count);
